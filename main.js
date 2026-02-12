2b0906d1cb90d705cc53d5ea1ac660c8844f9dcd
WycliffeCurson <wickycurson@gmail.com>
2026-02-12 15:53:58 +0300

    feat:added js code for the modal form

---
+++ main.js
@@ -0,0 +1,17 @@
+const loginBtn = document.querySelector('.login-btn');
+const modal = document.getElementById('loginModal');
+const closeBtn = document.getElementById('closeModal');
+
+loginBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
